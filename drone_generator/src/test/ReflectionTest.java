cf833eab4c1aead026d2a5834ed44b49a0f9596c
Hgyeol <neunimchi626@gmail.com>
2025-06-09 00:58:50 +0900

    added reflections library and modified test code

--- drone_generator/src/test/ReflectionTest.java
+++ drone_generator/src/test/ReflectionTest.java
@@ -1,12 +1,26 @@
 package test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.reflections.Reflections;
 
 import annotation.HangyeolController;
 import annotation.HangyeolMapping;
 
 public class ReflectionTest {
-	public static void main(String[] args) throws NoSuchMethodException, SecurityException {
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Reflections reflections = new Reflections("test");
+		Set<Class<?>> haveHangyeolControllerClass = reflections.getTypesAnnotatedWith(HangyeolController.class);
+//		Set<Class<?>> haveHangyeolControllerClass = new HashSet<>();
+//		haveHangyeolControllerClass.addAll(reflections.getTypesAnnotatedWith(HangyeolController.class));
+		for(Class<?> c : haveHangyeolControllerClass) {
+			System.out.println(c.getName());
+		}
+//		Set<Class<?>> haveHangyeolControllerClass = reflections.get(SubTypes.of(TypesAnnotated.with(HangyeolController.class)).asClass());
+		
 		TestController testController = new TestController();
 		
 		Method[] methods = testController.getClass().getMethods();
@@ -15,6 +29,19 @@ public class ReflectionTest {
 				HangyeolMapping hangyeolMapping = method.getAnnotation(HangyeolMapping.class);
 				String value = hangyeolMapping.value();
 				System.out.println("Method : " + method.getName() + ", Value : " + value);
+				
+				if(method.getName().equals("getHangyeol")) {
+					System.out.println("======Running Method======");
+					String result = (String) method.invoke(testController);
+					System.out.println("Result : " + result);
+				}
+				else {
+					System.out.println("======Running Method======");
+					
+					method.invoke(testController, "Fuck up");
+					String result = testController.getHangyeol();
+					System.out.println("Result : " + result);
+				}
 			}
 		}
 		
@@ -29,6 +56,42 @@ public class ReflectionTest {
 
 @HangyeolController
 class TestController {
+	
+	String hg = "hangyeol";
+	
+	public void hangyeol() {};
+	
+	@HangyeolMapping(value="1000")
+	public String getHangyeol() {
+		return hg;
+	}
+	
+	@HangyeolMapping(value="1234")
+	public void setHangyeol(String hg) {
+		this.hg = hg;
+	}
+}
+
+@HangyeolController
+class Test2Controller {
+	
+	String hg = "hangyeol";
+	
+	public void hangyeol() {};
+	
+	@HangyeolMapping(value="1000")
+	public String getHangyeol() {
+		return hg;
+	}
+	
+	@HangyeolMapping(value="1234")
+	public void setHangyeol(String hg) {
+		this.hg = hg;
+	}
+}
+
+@HangyeolController
+class Test3Controller {
 	
 	String hg = "hangyeol";
 	
